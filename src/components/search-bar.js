3e43725d994d9f5f870445ef2fb2b8ef600a5729
GastonArevalo <53384673+GastonArevalo@users.noreply.github.com>
2021-08-01 17:22:00 -0300

    usando state

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,41 +1,52 @@
-import React from 'react';
-import './search-bar.css'
-import logo from "../logo.svg"
+import React from "react";
+import "./search-bar.css";
+import logo from "../logo.svg";
 class SearchBar extends React.Component {
-    handleChange = e =>{
-        console.log('Este es el input y el evento es:', e.target.value)
-    }
-    handleClick = e =>{
-        e.preventDefault();
-        console.log(e.target.name, 'Evento click')
-    }
-    handleSubmit = e =>{
-        e.preventDefault();
-        console.log(e.target.name, e.target.value)
-    }
+  state = {
+    busqueda: "",
+  };
 
-    render() {
-        return (
-            <React.Fragment>
-                    <div className="row mt-2">
-                        <div className="col-md-2">
-                            <img src={logo} alt="logo" className="logo-barra" />
-                        </div>
-                        <div className="col-md-4">
-                            <form className="form-inline d-flex" name="formulario" onSubmit={this.handleSubmit}>
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log("Este es el input y el evento es:", e.target.value);
+  };
+  handleClick = (e) => {
+    e.preventDefault();
+    console.log(e.target.name, "Evento click");
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name, e.target.value);
+  };
 
-                                <div className="busqueda">
-                                    <input type="text" id="buscar" placeholder="Buscar"
-                                    onChange={this.handleChange}
-                                    />
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <hr />
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="row mt-2">
+          <div className="col-md-2">
+            <img src={logo} alt="logo FindRock" className="logo-barra" />
+          </div>
+          <div className="col-md-4">
+            <form
+              className="form-inline d-flex"
+              name="formulario"
+              onSubmit={this.handleSubmit}
+            >
+              <div className="busqueda">
+                <input
+                  type="text"
+                  id="buscar"
+                  placeholder="Buscar"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+        <hr />
+      </React.Fragment>
+    );
+  }
 }
 
 export default SearchBar;
